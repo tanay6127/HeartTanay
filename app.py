2c37ba05a70cc6a564956ad1014a72f3ff79815a
tanay6127 <maradanitanay@gmail.com>
2025-04-06 11:16:34 +0530

    app.py

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 import joblib
 st.title("Heart Disease Prediction by Tanay")
 model=joblib.load('ld.joblib')
-gender=st.number_input("Enter the Gender Male:0, Female:1 : ")
+sex=st.number_input("Enter the Gender Male:0, Female:1 : ")
 age=st.number_input("Enter the Age of the patient: ")
 cp=st.number_input("Enter the cp of the patient: ")
 trestbps=st.number_input("Enter the trestbps of the patient: ")
 if st.button("Predict to find out if the patient has a chance of Heart Disease"):
-    prediction=model.predict([[gender,age,cp,trestbps]])
+    prediction=model.predict([[sex,age,cp,trestbps]])
     if prediction=='1':
         st.text("Heart Disease may occur once go to the doctor")
     else:
